5a978cd6fb2350b608e8534367a857a9816863e3
Julio1230 <julio.ledezma_olivares@colostate.edu>
2024-12-29 20:39:14 -0700

    first phases

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-interface User {
-  id: number;
-  name: string;
-}
+import { Button } from "@chakra-ui/react";
 
 function App(){
 
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
-  const [isloading, setLoading] = useState(false);
-
-  useEffect (()  => {
-    setLoading(true);
-    axios
-    .get<User[]>('https://jsonplaceholder.typicode.com/usxers')
-    .then(response => {
-      setUsers(response.data);
-      setLoading(false);
-    })
-    .catch(error => {
-      setError(error.message);
-      setLoading(false);
-    });
-  }, [])
-
-  return <>
-  {error && <p className="text-danger">{error}</p>}
-  {isloading && <div className="spinner-border"></div>}
-  <ul>
-    {users.map(user => <li key={user.id}>{user.name}</li>)};
-  </ul>
-  </>
-
-
+  return <Button colorPalette={'blue'}>Button</Button>
 }
 
 export default App; 
