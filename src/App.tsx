40d319470bc12e6c9f882a36560fc95877b46255
Julio1230 <julio.ledezma_olivares@colostate.edu>
2025-01-22 16:02:32 -0700

    websi

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, Show, Center, Box } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
 function App(){
 
   return <Grid templateAreas={{base: '"nav" "main"', lg: '"nav nav" "aside main"'}}>
     <GridItem area='nav' bg='black'><NavBar /></GridItem>
-    <GridItem area='aside' bg='gold' display={{ base: "none", lg: "block" }}>Aside</GridItem>
     <GridItem area='main' bg='dodgerblue'>Main</GridItem>
   </Grid>
 }
